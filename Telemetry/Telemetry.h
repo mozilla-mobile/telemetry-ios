ed4bb893359b6a9ff78bf8b0b658837b2b87060b
Justin D'Arcangelo <justindarc@gmail.com>
2017-03-13 17:24:51 -0400

    Added new Xcode project.

---
+++ Telemetry/Telemetry.h
@@ -0,0 +1,19 @@
+//
+//  Telemetry.h
+//  Telemetry
+//
+//  Created by Justin D'Arcangelo on 3/13/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Telemetry.
+FOUNDATION_EXPORT double TelemetryVersionNumber;
+
+//! Project version string for Telemetry.
+FOUNDATION_EXPORT const unsigned char TelemetryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Telemetry/PublicHeader.h>
+
+
